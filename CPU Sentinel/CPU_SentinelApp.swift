0b3364c93d0a0d816310a1f376258b209c82ce2f
Kursat Burak Fariz <kburakfariz@gmail.com>
2026-04-06 15:04:23 +0300

    Initial Commit

---
+++ CPU Sentinel/CPU_SentinelApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPU_SentinelApp.swift
+//  CPU Sentinel
+//
+//  Created by Kürşat Burak Farız on 6.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct CPU_SentinelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPU SentinelTests/CPU_SentinelTests.swift
@@ -0,0 +1,17 @@
+//
+//  CPU_SentinelTests.swift
+//  CPU SentinelTests
+//
+//  Created by Kürşat Burak Farız on 6.04.2026.
+//
+
+import Testing
+@testable import CPU_Sentinel
+
+struct CPU_SentinelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
